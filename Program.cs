23521cd2f0e4e21fe4f7317933f544cf58945e0c
Anders Bo Pedersen <ABP@ao.dk>
2023-04-17 13:47:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace tråd2
+{
+    internal class Program
+    {
+        static char ch = '*';
+        static void Main(string[] args)
+        {
+            Thread p = new Thread(output);
+            p.Name = "Print";
+            p.Start();
+
+
+            Thread r = new Thread(input);
+            r.Name = "Reader";
+            r.Start();
+
+            p.Join();
+            r.Join();
+
+            Console.ReadKey();
+
+        }
+
+        static void input()
+        {
+            while (true)
+            {
+                ch = Console.ReadKey().KeyChar;
+            }
+        }
+
+        static void output()
+        {
+            while (true)
+            {
+                Thread.Sleep(500);
+                Console.Write(ch);
+            }
+        }
+    }
+}
